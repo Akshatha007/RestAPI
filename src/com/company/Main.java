ea843bc23fee015b8989064f19f6f518095e3373
akshatha007 <akshatha191294@gmail.com>
2022-08-04 13:45:11 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		String Name ;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name:");
+		Name = sc.nextLine();
+		returningTheName(Name);
+	}
+	
+	public static void returningTheName(String Name)
+	{
+		System.out.println(Name);
+	}
+}
